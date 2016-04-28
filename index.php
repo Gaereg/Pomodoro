f4f36f35436f6f700e65ff75f0882f900fba5d00
Mathieu <larrieu.mathieu.v@gmail.com>
2016-04-28 14:41:18 +0200

    Liaison serveur Ok

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION['nbTaff']) && !isset($_SESSION['repo'])){
+        $_SESSION['nbTaff'] = 0;
+        $_SESSION['repo'] = 0;
+    }
+
+    session_start();
+
+    try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '741741');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+
+ ?>
+
+<!doctype html>
+<html lang="fr" ng-app="pomoApp">
+<head>
+    <!--Meta-->
+    <meta charset = "UTF-8">
+    <meta name    = "author" content      = "Gaereg">
+    <meta name    = "description" content = "Pomodoro">
+    <!--link externe-->
+    <link rel     = "stylesheet" href     = "bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <script src = "bower_components/angular/angular.min.js"></script>
+
+    <!-- Font -->
+
+    <!--link interne-->
+    <link rel     ="stylesheet" href="style/master.css">
+    <script src = "js/app.js"></script>
+    <script src = "js/controller.js"></script>
+
+    <script>
+        window.sessionTaff = parseInt("<?php echo $_SESSION['nbTaff'] ?>");
+        var tempRepo = parseInt("<?php echo $_SESSION['repo'] ?>");
+        if(tempRepo == 0){
+            window.sessionRepo = false;
+        } else {
+            window.sessionRepo = true;
+        }
+
+        function isRepo(){
+            "<?php $_SESSION['repo'] = 0 ?>"
+        }
+    </script>
+    <!--Titre-->
+    <title>Pomodoro</title>
+
+</head>
+
+<body>
+    <header>
+    </header>
+
+    <main ng-controller="MainCtrl">
+        <div class="container">
+            <h1>My pomodoro app</h1>
+            <h2>Doing</h2>
+                {{nbTaff}}
+                {{repo}}
+            <h3>Languages :</h3>
+            <div class="langages">
+                <div class="icon-name" ng-repeat="language in languages" ng-click="languageSelect()">
+                    <img src="{{language.logo}}" alt="logo {{language.name}}">
+                    <p ng-class="{select: language.select}">{{language.name}}</p>
+                </div>
+            </div>
+            <input class="language-autre" ng-model="languageInput" ng-blur="languageAutre()" ng-hide="nbLanguage >= 2 && languageInput == ''" type="text" placeholder="autre" maxlength="50">
+
+            <form ng-submit="play()">
+                <div class="form-group">
+
+                    <label for="task"></label>
+                    <input type="text" id="task" class="form-control" ng-model="task" autocomplete="off" placeholder="Commentaire" maxlength="200">
+                </div>
+            </form>
+            <div id="controls">
+                <p class="time">{{timer.minutes}}:{{timer.secondes | numberFixedLen:2}} {{work}}</p>
+                <button type="button" class="btn btn-default btn-lg" ng-click="play()">
+                    <span class="glyphicon glyphicon-play" aria-hidden="true"></span>
+                </button>
+                <button type="button" class="btn btn-default btn-lg" ng-click="pause()">
+                    <span class="glyphicon glyphicon-pause" aria-hidden="true"></span>
+                </button>
+                <button type="button" class="btn btn-default btn-lg" ng-click="stop()">
+                    <span class="glyphicon glyphicon-stop" aria-hidden="true"></span>
+                </button>
+            </div>
+            <h2>Done {{heureTaf}}h{{minTaf}}</h2>
+            <ul id="done" class="list-group">
+
+            </ul>
+        </div>
+    </main>
+
+    <footer>
+    </footer>
+    <!-- Script -->
+</body>
+</html>
--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,17 @@
 app.controller('MainCtrl', function($scope, $interval) {
+    $scope.repo = sessionRepo;
+    $scope.nbTaff = sessionTaff;
+
     $scope.timer = { // Object timer avec ses méthode
         secondes: 3,
-        minutes: 0,
-        nbTaff: 0,
-        repo: false
+        minutes: 0
     };
+
+    if($scope.repo === true && $scope.nbTaff%4 === 0){
+        $scope.timer.minutes = 20;
+    } else if ($scope.repo === true){
+        $scope.timer.minutes = 5;
+    }
 
     $scope.languages = [
         {name: 'CSS', logo:     'images/css3.png', select:    false},
@@ -23,6 +30,9 @@ app.controller('MainCtrl', function($scope, $interval) {
     $scope.work = '';
     $scope.heureTaf = 0;
     $scope.minTaf = 0;
+
+    $scope.heureTaf = parseInt($scope.nbTaff*25/60,10); //Calcule le temps passé a travailler
+    $scope.minTaf = $scope.nbTaff*25 - $scope.heureTaf*60;
 
     $scope.languageSelect = function(){
         if($scope.nbLanguage < 2 && this.language.select === false){
@@ -50,25 +60,25 @@ app.controller('MainCtrl', function($scope, $interval) {
            $scope.timer.secondes = 59;
         } else if ($scope.timer.secondes > 0 && $scope.timer.minutes >= 0) { // Sinon seconde -1
             --$scope.timer.secondes;
-        } else if ($scope.timer.secondes === 0 && $scope.timer.minutes === 0 && $scope.timer.repo === false) { //Quand le timer travail est fini
-            var lang = '';
+        } else if ($scope.timer.secondes === 0 && $scope.timer.minutes === 0 && $scope.repo === false) { //Quand le timer travail est fini
+            var langLogo = '';
+            var langName = '';
             for(i=0; i<$scope.languages.length; i++){
                 if($scope.languages[i].select === true){
-                    lang += ' '+"<img src='"+$scope.languages[i].logo+"'>";
+                    langLogo += ' '+"<img src='"+$scope.languages[i].logo+"'>";
+                    langName += $scope.languages[i].name+' ';
                 }
             }
             if($scope.languageInput !== ''){
-                lang += ' '+$scope.languageInput;
+                langLogo += ' '+$scope.languageInput;
+                langName += $scope.languages[i].name+' ';
             }
-            console.log(lang);
-            done.prepend("<li class='list-group-item tache-done'><div>"+lang+'</div><s><div>'+$scope.task+"</div></s></li>");
-            $scope.timer.nbTaff++;
-            $scope.heureTaf = parseInt($scope.timer.nbTaff*25/60,10); //Calcule le temps passé a travailler
-            $scope.minTaf = $scope.timer.nbTaff*25 - $scope.heureTaf*60;
-            console.log($scope.heureTaf, $scope.minTaf);
-            $scope.timer.repo = true;
+            $scope.nbTaff++;
+            $scope.repo = true;
+            window.location.assign('php/inject.php?nbTaff='+$scope.nbTaff+'&lang='+langName+'&task='+$scope.task);
 
-            if ($scope.timer.nbTaff%4 === 0){ // Calcul le temps de pause en fonction du nombre de session de travaille faite, et lance diret la pause
+
+            if ($scope.nbTaff%4 === 0){ // Calcul le temps de pause en fonction du nombre de session de travaille faite, et lance diret la pause
                 $scope.timer.minutes = 20;
                 alert('20 minutes de pause');
             } else {
@@ -80,10 +90,11 @@ app.controller('MainCtrl', function($scope, $interval) {
         } else { // si timer pause fini, stop le timer
             alert('Fini la pause');
             $scope.stop();
-            $scope.timer.repo = false;
+            $scope.repo = false;
+            isRepo();
         }
 
-        if ($scope.timer.repo){ //Créer une chaine de caractère en fonction de si on est au boulot ou en pause
+        if ($scope.repo){ //Créer une chaine de caractère en fonction de si on est au boulot ou en pause
             $scope.work = 'Pause';
         } else {
             $scope.work = 'Boulot';
@@ -92,7 +103,6 @@ app.controller('MainCtrl', function($scope, $interval) {
     };
     var playTime;
     $scope.play = function(){ //Fait boucler le timer
-        console.log('e');
         if(!$scope.isPlay && $scope.task !== '' && $scope.nbLanguage > 0){
             playTime = $interval(function(){ $scope.time(); }, 1000);
             $scope.isPlay = true;
@@ -111,5 +121,9 @@ app.controller('MainCtrl', function($scope, $interval) {
         $scope.timer.secondes = 0;
         $scope.isPlay = false;
         $interval.cancel(playTime);
+    };
+
+    $scope.reprise = function(){
+        $scope.nbTaff = test;
     };
 });
